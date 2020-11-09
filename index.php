e6a1a364162c6fb331cc233dec9481dbbea6e73a
Renato S. Oliveira <renato.santojpg@gmail.com>
2020-11-09 19:51:14 -0300

    Adicionando index.php para carregar os recursos

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 
-header('Content-Type: text/html; charset=utf-8');
-
 use Library\Autoload as Autoload;
 use Library\Router as Router;
 
 define('DIR_ROOT', __DIR__);
 define('DS', DIRECTORY_SEPARATOR);
 
-require_once DIR_ROOT . DS . './Library/Autoload.php';
+require_once DIR_ROOT . DS . 'Library/Autoload.php';
 
 $autoload = new Autoload();
-// $router = new Router();
+$router = new Router();
