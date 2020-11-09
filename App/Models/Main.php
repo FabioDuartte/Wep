39f51480055e761a0099828ef5611de235608f39
Renato S. Oliveira <renato.santojpg@gmail.com>
2020-11-09 19:55:06 -0300

    ARQUIVO PARA TESTEgit add .

--- App/Models/Main.php
+++ App/Models/Main.php
@@ -6,9 +6,27 @@ use App\Controllers\MasterController;
 
 require_once '../../index.php';
 
+// Estava pensando em alterar os nomes dos arquivos!!
+// Temos que padronizar a interface.
+// Tem mesmo que ter a barra de busca?
+// Fábio precisa arrumar a parte do Gerente
+
+/* Métodos que devo Fazer
+    
+*/
+
 $name = 'Renato';
 
 $MC = new MasterController();
 
-$client = new Client($name, '050', '@@ola', 'kkOla881244', '33451321');
+$Customer = new Customer($name, '050', '@@ola', 'kkOla881244', '33451321', '8888', '777', '2525',);
+$food = new MenuFoods("Panquecas", "5.90", "12", array("3 Ovos", "Pimenta do reino", "Cebola", "Salsicha", "Coentro", "Sal"));
+$drink = new MenuDrinks("Suco de Laranja", "2.50", "6", "Do Vale");
 
+
+$order = new Order($Customer, array($food, $drink));
+echo $order->getTotalOrderAmount();
+echo "<br>";
+echo $order->getOrderingCustomer()->getUserName();
+
+// echo $Customer->getCustomerCard()->getCardExpiry();
